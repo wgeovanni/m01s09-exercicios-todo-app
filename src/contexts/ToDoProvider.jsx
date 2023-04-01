f7ae814a75cf3fd19610b2ccac3de325791de2ba
wgeovanni <willyangeo@gmail.com>
2023-03-31 23:03:17 -0300

    Exercicio5

--- src/contexts/ToDoProvider.jsx
+++ src/contexts/ToDoProvider.jsx
@@ -1,3 +1,11 @@
 import { createContext } from "react";
 
 export const todoContext = createContext()
+
+export const ToDoProvider = ({ children }) => {
+    return (
+        <todoContext.Provider value={""}>
+            {children}
+        </todoContext.Provider>
+    )
+}
